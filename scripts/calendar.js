cbac5f3f9d3c0b3b612616426a2cbf3ab619e031
migueleonrojasmetapagos <migueleonrojas@gmail.com>
2024-07-21 09:00:30 -0400

    fix calendar days of month

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -3,38 +3,28 @@ import getNameOfMonth from "../scripts/get-name-moth.js"
 const d = document;
 
 const renderDayOfMonth = (start, end, start_selected, end_selected) => {
+
    let $fragment = document.createDocumentFragment();
    let $main_information_calendar_content = document.querySelector(".main-information_calendar_content");
    d.querySelectorAll(".main-information_calendar_number_of_week").forEach(el => el.remove());
-   for(let i = 0; i <= end + 1; i++) {
+   for(let i = -start; i <= end; i++) {
 
-      let content;
-      let el_class;
-      
-      if(i < start) {
-         el_class = [
-            "main-information_calendar_number_of_week",
-            "pi-none"
-         ]
-         content = "";
-      } 
-      else {
-         el_class = ["main-information_calendar_number_of_week"];
-         content = i;
-      }
-      if(i >= start_selected && i <= end_selected) {
-         el_class = [
-            "main-information_calendar_number_of_week",
-             "main-information_calendar_number_of_week_selected"
-         ];
-      }
-
-   
       let $main_information_calendar_number_of_week = document.createElement("span");
-      $main_information_calendar_number_of_week.classList.add(...el_class);
-      $main_information_calendar_number_of_week.innerHTML = content;
+      
+      if(i >= 1) {
+         $main_information_calendar_number_of_week.classList.add("main-information_calendar_number_of_week");
+         $main_information_calendar_number_of_week.innerHTML = i;
+
+
+         if(i >= start_selected && i <= end_selected) {
+            $main_information_calendar_number_of_week.classList.add("main-information_calendar_number_of_week_selected");
+         }
+
+      }
+      
       let $clone = document.importNode($main_information_calendar_number_of_week, true);
-      $fragment.appendChild($clone);
+      $fragment.appendChild($clone); 
+
    }
 
    $main_information_calendar_content.appendChild($fragment);
@@ -44,11 +34,11 @@ const renderCalendar = (start_date, end_date) => {
 
    const $main_information_calendar_name_month = d.querySelector(".main-information_calendar_name_month");
 
-   const sd = new Date(start_date.seconds * 1000)
-   const ed = new Date(end_date.seconds * 1000)
+   const sd = new Date(start_date['seconds'] * 1000);
+   const ed = new Date(end_date['seconds'] * 1000);
 
-   const days_of_moth = new Date(sd.getFullYear(), sd.getMonth(), 0).getDate();
-   const day_of_week_start = new Date(sd.getFullYear(), sd.getMonth(), 1).getDay();
+   const days_of_moth = new Date(sd.getFullYear(), sd.getMonth() + 1, 0).getDate();
+   const day_of_week_start = new Date(sd.getFullYear(), sd.getMonth(), 0).getDay();
    const day_sd = sd.getDate();
    const month_sd = sd.getMonth() + 1;
    const year_sd = sd.getFullYear();
