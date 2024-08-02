f1eddbe2e7dd2a24b7bce3087f84cae4c8c1cee3
migueleonrojasmetapagos <migueleonrojas@gmail.com>
2024-08-01 20:01:51 -0400

    fix input type number

--- scripts/build-inputs-code.js
+++ scripts/build-inputs-code.js
@@ -7,13 +7,13 @@ const buildInputsCode = (classInput) => {
       el.addEventListener("input", (e) => {
 
          let value = e.target.value.replace(/[^0-9]/g,'');
-         e.target.value = value;
+         e.target.value = value.substring(0,1);
 
          if(value === "" && e.target.previousElementSibling !== null) {
             e.target.previousElementSibling.focus();
          }
 
-         if(value.length >= 1) {
+         if(value.length === 1) {
 
             if(e.target.nextElementSibling !== null){
                e.target.nextElementSibling.focus();
