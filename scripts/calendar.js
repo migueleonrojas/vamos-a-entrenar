2548e12edd71c3382355508987c0e6f7fb12db45
migueleonrojasmetapagos <migueleonrojas@gmail.com>
2024-08-31 21:36:13 -0400

    fix calendar range month for 2 months

--- scripts/calendar.js
+++ scripts/calendar.js
@@ -2,11 +2,9 @@ import getNameOfMonth from "../scripts/get-name-moth.js"
 
 const d = document;
 
-const renderDayOfMonth = (start, end, start_selected, end_selected) => {
+const renderDayOfMonth = (start, end, start_selected, end_selected, selectorCalendarContent) => {
 
    let $fragment = document.createDocumentFragment();
-   let $main_information_calendar_content = document.querySelector(".main-information_calendar_content");
-   d.querySelectorAll(".main-information_calendar_number_of_week").forEach(el => el.remove());
    for(let i = -start; i <= end; i++) {
 
       let $main_information_calendar_number_of_week = document.createElement("span");
@@ -28,7 +26,7 @@ const renderDayOfMonth = (start, end, start_selected, end_selected) => {
       $fragment.appendChild($clone); 
 
    }
-   $main_information_calendar_content.appendChild($fragment);
+   selectorCalendarContent.appendChild($fragment);
 }
 
 const renderCalendar = (start_date, end_date) => {
@@ -47,11 +45,39 @@ const renderCalendar = (start_date, end_date) => {
    const month_ed = ed.getMonth() + 1;
    const year_ed = ed.getFullYear();
 
-   const nameMoth = getNameOfMonth(month_sd);
+   d.querySelectorAll(".main-information_calendar_number_of_week").forEach(el => el.remove());
+   if(d.querySelectorAll(".main-information_calendar_content")[1] !== undefined) {
+      d.querySelectorAll(".main-information_calendar_content")[1].remove();
+   }
+   
+   if((day_sd < day_ed) || (day_sd === day_ed)) {
+      const nameMoth = getNameOfMonth(month_sd);
+      renderDayOfMonth(day_of_week_start, days_of_moth, day_sd, day_ed, document.querySelector(".main-information_calendar_content"));
+      $main_information_calendar_name_month.innerHTML = nameMoth; 
+   }
 
-   renderDayOfMonth(day_of_week_start, days_of_moth, day_sd, day_ed);
- 
-   $main_information_calendar_name_month.innerHTML = nameMoth; 
+   else {
+      const nameMoth = getNameOfMonth(month_sd);
+      renderDayOfMonth(day_of_week_start, days_of_moth, day_sd, days_of_moth, document.querySelector(".main-information_calendar_content"));
+      $main_information_calendar_name_month.innerHTML = nameMoth; 
+
+      const $anotherCalendar = d.querySelector(".main-information_calendar_content").cloneNode(true);
+      $anotherCalendar.querySelectorAll(".main-information_calendar_number_of_week").forEach(el => el.remove());
+
+
+      const another_days_of_moth = new Date(sd.getFullYear(), sd.getMonth() + 2, 0).getDate();
+      const another_day_of_week_start = new Date(sd.getFullYear(), sd.getMonth() + 1, 0).getDay();
+      
+      const nextNameMoth = getNameOfMonth(month_sd + 1 === 13 ? 1: month_sd);
+      renderDayOfMonth(another_day_of_week_start, another_days_of_moth, 1, day_ed, $anotherCalendar);
+      $anotherCalendar.querySelector(".main-information_calendar_name_month").innerHTML = nextNameMoth;
+
+
+      d.querySelector(".main-information_calendar").insertAdjacentElement("beforeend", $anotherCalendar)
+
+
+   }
+
    
 }
 
